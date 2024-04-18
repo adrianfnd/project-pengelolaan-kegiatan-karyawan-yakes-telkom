c772aeac7278e8373b1507c851531b956fe273ba
adrianfnd <fernanda.adrian2@gmail.com>
2024-04-18 23:43:54 +0700

    fixing home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,26 +13,12 @@ class HomeController extends Controller
     public function dashboard()
     {
         $total_data = Pelatihan::count();
-    
-        // Menghitung data pelatihan hari ini
         $data_hari_ini = Pelatihan::whereDate('tgl_mulai', date('Y-m-d'))->count();
-    
-        // Menghitung data pelatihan internal
         $data_internal = Pelatihan::where('jenis_pelatihan', 'Internal')->count();
-    
-        // Menghitung data pelatihan publik
         $data_publik = Pelatihan::where('jenis_pelatihan', 'Publik')->count();
-    
-        // Menghitung data pelatihan dengan eviden "Daftar hadir"
         $data_daftar_hadir = Pelatihan::where('eviden', 'Daftar hadir')->count();
-    
-        // Menghitung data pelatihan dengan eviden "Sertifikat"
         $data_sertifikat = Pelatihan::where('eviden', 'Sertifikat')->count();
-    
-        // Menghitung data pelatihan dengan eviden "Notadinas"
         $data_notadinas = Pelatihan::where('eviden', 'Notadinas')->count();
-    
-        // Menghitung data pelatihan dalam 7 hari terakhir
         $start_date = now()->subDays(7)->startOfDay();
         $end_date = now()->endOfDay();
         $data_7_days = Pelatihan::whereBetween('tgl_mulai', [$start_date, $end_date])
